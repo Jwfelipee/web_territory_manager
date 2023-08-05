a88f2dbe20d8fb02b16df48b42ad884a4e8b374a
JWFelipe <joao.silva@tegra.com.br>
2023-08-04 21:04:21 -0300

    fix: crash build

--- src/modules/misc/routes/territory/components/SearchButton.tsx
+++ src/modules/misc/routes/territory/components/SearchButton.tsx
@@ -1,12 +1,11 @@
 import { Button } from "@/components/ui";
-import { ISetSearch } from "../type";
 import { Search } from "react-feather";
 import clsx from "clsx";
 
-export const SearchButton = ({ setSearch }: { setSearch: ISetSearch }) => {
+export const SearchButton = () => {
   return (
     <Button.Root
-      onClick={() => setSearch((prev) => ({ ...prev, show: !prev.show }))}
+      // onClick={() => setSearch((prev) => ({ ...prev, show: !prev.show }))}
       className={clsx(
         "fixed bottom-6 -translate-x-1/2 left-1/2 !w-12 !h-12 !p-2 !rounded-full animate-pulse"
       )}
